efc4b15d826d55dc4f1da16082a529dab25b04e9
wellington <wellyngtonqueirozs@gmail.com>
2022-08-02 23:14:24 -0300

    initial

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,12 @@
-import { useState } from 'react'
 import Header from './Components/Header'
-
 import AddArea from './Components/AddArea'
 
-function App() {
-  const [count, setCount] = useState(0)
 
+const App = ()=> {
+  
   return (
     <div className='bg-gradient-to-r bg-darkPrimary min-h-screen p-5'>
-     <div className='m-auto max-w-screen-md '>
+     <div className='m-auto max-w-screen-md'>
         <Header/>
       <AddArea/>
      </div>
