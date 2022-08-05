579893ab22f300c086d100567b927409a2fd2e65
wellington <wellyngtonqueirozs@gmail.com>
2022-08-05 10:23:43 -0300

    add task

--- src/Components/AddArea/index.tsx
+++ src/Components/AddArea/index.tsx
@@ -1,54 +1,56 @@
-import { FormEvent, useState, ChangeEvent } from "react"
+import { FormEvent, useState, ChangeEvent } from 'react'
 
-import { ITask } from "../Interface"
-import CardTask from "../CardTask"
-import Input from "../Input";
+import { ITask } from '../Interface'
+import CardTask from '../CardTask'
+import Input from '../Input'
 
-const LIMIT = 999;
-const AddArea = ()=>{
+const LIMIT = 999
+const AddArea = () => {
+  const [task, setTask] = useState('')
+  const [todoList, setTodoList] = useState<ITask[]>([])
 
-    const [task, setTask]= useState('')
-    const [todoList, setTodoList]= useState<ITask[]>([])
+  const addTask = (e: FormEvent) => {
+    e.preventDefault()
+    const idRandom = (num: number) => Math.floor(Math.random() * num)
 
-    const addTask =(e: FormEvent)=>{
-        e.preventDefault()
-        const idRandom  = (num: number)=> Math.floor(Math.random() * num);
-     
-       const newTask = {id: idRandom(LIMIT), nameTask:task, done: true}
-       setTodoList([...todoList, newTask])
-       setTask('')
-    }
+    const newTask = { id: idRandom(LIMIT), nameTask: task, done: true }
+    setTodoList([...todoList, newTask])
+    setTask('')
+  }
 
-    const handleChange = (e : ChangeEvent<HTMLInputElement>)=>{
-        setTask(e.target.value)
-    }
+  const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+    setTask(e.target.value)
+  }
 
-    const deleteTask = (DeleTaskId: number)=>{
-        setTodoList(todoList.filter((item)=> item.id !== DeleTaskId))
-    }
+  const deleteTask = (DeleTaskId: number) => {
+    setTodoList(todoList.filter(item => item.id !== DeleTaskId))
+  }
 
-    const checkTodoList = (id:number)=>{
-        const checkTask = todoList.map((items)=>{
-            if(items.id === id){
-                return {...items, done: !items.done}
-            }
-            return items
-        })
-            setTodoList(checkTask)
-    }
+  const checkTodoList = (id: number) => {
+    const checkTask = todoList.map(items => {
+      if (items.id === id) {
+        return { ...items, done: !items.done }
+      }
+      return items
+    })
+    setTodoList(checkTask)
+  }
 
-    return(
-        <div>
-            <Input handleSubmite={addTask}  handleChange={handleChange} task={task} />
-			
-                {todoList.map((item)=>(
-                    <section key={item.id}>
-                        <CardTask checkTodoList={checkTodoList} task={item} deleteTask={deleteTask} />
-                    </section>
-                ))}
+  return (
+    <div>
+      <Input handleSubmite={addTask} handleChange={handleChange} task={task} />
 
-        </div>
-    )
+      {todoList.map(item => (
+        <section key={item.id}>
+          <CardTask
+            checkTodoList={checkTodoList}
+            task={item}
+            deleteTask={deleteTask}
+          />
+        </section>
+      ))}
+    </div>
+  )
 }
 
 export default AddArea
