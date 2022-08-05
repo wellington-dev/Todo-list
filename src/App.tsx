130c673af3f51f6f93163281ca10bc9b31d672c0
wellington <wellyngtonqueirozs@gmail.com>
2022-08-04 21:16:50 -0300

    initial

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,13 @@
-import Header from './Components/Header'
+import Header from "./Components/Header"
 import AddArea from './Components/AddArea'
 
-
-const App = ()=> {
-  
+const App = () => {
   return (
-    <div className='bg-gradient-to-r bg-darkPrimary min-h-screen p-5'>
-     <div className='m-auto max-w-screen-md'>
-        <Header/>
-      <AddArea/>
-     </div>
+    <div className="bg-gradient-to-r bg-darkPrimary min-h-screen p-5">
+      <div className="m-auto max-w-screen-md">
+        <Header />
+        <AddArea />
+      </div>
     </div>
   )
 }
